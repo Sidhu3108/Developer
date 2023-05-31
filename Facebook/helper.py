7fb9e2e7ea8981d2d0d6cd3e1151109b9810d933
yamini <yaminimunakala29@gmail.com>
2023-05-31 22:37:18 +0530

    changes are done

--- Facebook/helper.py
+++ Facebook/helper.py
@@ -2,4 +2,3 @@ from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 from webdriver_manager.chrome import ChromeDriverManager
-import time
